e349396aee9262df01729a9121c7f0d1e48fd06b
Yen <62843535+User25514@users.noreply.github.com>
2021-08-16 11:39:27 +0100

    Made Smaller

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 class Func:
-    def loopCalc(self, equation):
+    def loopCalc(self):
+        equation = str(input("Equation: "))
         split = equation.split(" ")
         counter = 0
         total = float(0)
@@ -7,45 +8,28 @@ class Func:
         total = int(split[0])
         for x in split[1:len(split)]:
             if x.isnumeric() == True:
-                symbol, total = Filter.numberFilter(0, symbol, total, x)
-            elif x in ("+-/**"):
+                symbol, total = Filter.numberFilter(0, symbol, total, int(x))
+            elif x in ("+-//**"):
                 symbol = Filter.symbolFilter(0, x)
-        messages.Total(0, equation, total)
-
-    def pluss(self, total, x):
-        return total + int(x)
-
-    def divide(self, total, x):
-        return total / int(x)
-
-    def times(self, total, x):
-        return total * int(x)
-
-    def subtract(self, total, x):
-        return total - int(x)
-
-    def power(self, total, x):
-        return total ** int(x)
+        print(f"{equation} = {total}")
 
 
 class Filter():
     def numberFilter(self, symbol, total, x):
         if not(symbol == 0):
             if symbol == 1:
-                total = Func.pluss(0, total, x)
-                symbol = 0
+                total += x
             elif symbol == 2:
-                total = Func.divide(0, total, x)
-                symbol = 0
+                total /= x
             elif symbol == 3:
-                total = Func.times(0, total, x)
-                symbol = 0
+                total *= x
             elif symbol == 4:
-                total = Func.subtract(0, total, x)
-                symbol = 0
+                total -= x
             elif symbol == 5:
-                total = Func.power(0, total, x)
-                symbol = 0
+                total **= x
+            elif symbol == 6:
+                total //= x
+        symbol = 0
         return symbol, total
 
     def symbolFilter(self, x):
@@ -59,13 +43,8 @@ class Filter():
             symbol = 4
         elif x == "**":
             symbol = 5
+        elif x == "//":
+            symbol = 6
         return symbol
-
-
-class messages():
-    def Total(self, equation, total):
-        print(f"{equation} = {total}")
-
-
-equation = str(input("Equation: "))
-Func.loopCalc(0, equation)
+if __name__ == "__main__":
+    Func.loopCalc(0)
